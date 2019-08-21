a63e9983ac3c859a6d5789d0cdd10ebdc41c6d7a
nkulow <nkulow@gmail.com>
2019-08-21 13:55:13 -0400

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Header.css'
 import MFS_Logo from '/Users/Kulow/React/myflood-score/src/images/MFS_Logo2_572019-2.png'
+import Compare from '/Users/Kulow/React/myflood-score/src/images/Compare.svg'
+import Examine from '/Users/Kulow/React/myflood-score/src/images/Examine.svg'
+import Certify from '/Users/Kulow/React/myflood-score/src/images/Certificate.svg'
+import Reduce_Eliminate from '/Users/Kulow/React/myflood-score/src/images/Reduce_Eliminate.svg'
+import Discover from '/Users/Kulow/React/myflood-score/src/images/Discover.svg'
 import NavBar from 'react-bootstrap/Navbar'
 import {Row, Col} from 'react-bootstrap'
+import shoppingCart from '/Users/Kulow/React/myflood-score/src/images/shopping-cart-solid.svg'
 import {
     Container,
     Grid,
@@ -19,13 +25,14 @@ const useStyles = makeStyles(theme => ({
     },
     fontFamily: '"Montserrat", sans-serif'
 }));
+
 function Header () {
     const classes = useStyles();
     return (
     <div>
     <div className={'Headercontainer'}>
         <Row >
-            {/* <NavBar expand='lg'> */}
+            <NavBar expand='lg'>
                 <span className={'header'}>
                     <Col>
                         <img src={MFS_Logo} className={'MFS-Logo'} alt={''}/>
@@ -53,18 +60,67 @@ function Header () {
                         <a href="https://reactjs.org" className={'link'}>
                             Sign Up
                         </a>
+                        <a href="https://reactjs.org" className={'link'}>
+                            <img src={shoppingCart} className={'cart'} alt={''}/>
+                        </a>
                     </Grid>
                 </span>
-            {/* </NavBar>    */}
+            </NavBar>   
         </Row>
         </div>
-        <Row>
-            <Grid>
-                <Button 
-                    className={classes.button}
+        <Row className='bottomBorder'>
+            <Grid 
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    style={{paddingTop: 20, marginBottom: 10}}
+                >
+                <a
+                    className={'links2'}
+                    href="https://reactjs.org"
                 >
                     Discover
-                </Button>
+                </a>
+                <a href="https://reactjs.org">
+                    <img src={Discover} className={'links2logo'} alt={''}/>
+                </a>
+                <a
+                    className={'links2'}
+                    href="https://reactjs.org"
+                >
+                    Compare
+                </a>
+                <a href="https://reactjs.org">
+                    <img src={Compare} className={'links2logo'} style={{marginLeft: -40, width: 120, height: 40}} alt={''}/>
+                </a>
+                <a
+                    className={'links2'}
+                    href="https://reactjs.org"
+                >
+                    Examine
+                </a>
+                <a href="https://reactjs.org">
+                    <img src={Examine} className={'links2logo'} alt={''}/>
+                </a>
+                <a
+                    className={'links2'}
+                    href="https://reactjs.org"
+                >
+                    Certify
+                </a>
+                <a href="https://reactjs.org">
+                    <img src={Certify} className={'links2logo'} alt={''}/>
+                </a>
+                <a
+                    className={'links2'}
+                    href="https://reactjs.org"
+                >
+                    Reduce or Eliminate
+                </a>
+                <a href="https://reactjs.org">
+                    <img src={Reduce_Eliminate} className={'links2logo'} alt={''}/>
+                </a>
             </Grid>
         </Row>
         
