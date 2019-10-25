004449f49331009e85a66a17a5f75e2905cf4927
Cody Karunas <codykarunas@gmail.com>
2019-10-25 15:18:46 -0400

    Added the background image and a dark gray linear gradient overlay. Tweaked font sizes.

--- src/containers/Home/Testimonials/Testimonials.js
+++ src/containers/Home/Testimonials/Testimonials.js
@@ -8,8 +8,29 @@ import Col from 'react-bootstrap/Col'
 import './Testimonials.css'
 
 export default function Testimonials() {
+  const [nextIcon, setNextIcon] = useState(<span aria-hidden="true" className="carousel-control-next-icon" />)
+  const [prevIcon, setPrevIcon] = useState(<span aria-hidden="true" className="carousel-control-prev-icon" />)
+  const [testimonialTitle, setTestimonialTitle] = useState('What people say about our flood risk intelligence.')
+  const [testimonials, setTestimonials] = useState([
+    {
+      body: "I'm a Realtor and I like what you're doing. I think this is a huge benefit to those clients who have already made a decision to purchase a property within a floodzone. Being able to give my buyer's a detailed report like this is definitely an added value service I can provide for them. Good job guys!!!",
+      author: "M S Rivard",
+      location: "Tampa FL"
+    },
+    {
+      body: "I cannot thank you enough for this, it gives me great relief of mind that my wife and I chose the right place. Thank you!",
+      author: "T Weisman",
+      location: "St Petersburg FL"
+    },
+    {
+      body: "We thank you for your ingenuity and foresight to help with a necessary evil that has become very cloudy over the years... and it is refreshing to hear someone put clarity and direction on this topic.",
+      author: "R L Hord",
+      location: "Sarasota FL"
+    }
+  ])
+
   return (
-    <Carousel className="carousel">
+    <Carousel className="carousel" nextIcon={nextIcon} prevIcon={prevIcon}>
       <CarouselItem className="carousel-item">
         <img
           className="d-block w-100"
@@ -20,7 +41,7 @@ export default function Testimonials() {
           <div>
             <div className="carousel-caption-header">
               What people say about our flood risk intelligence.
-              </div>
+            </div>
             <hr style={{ border: "1px solid rgb(85, 185, 106)" }} />
             <div className="carousel-caption-body">
               <p>
