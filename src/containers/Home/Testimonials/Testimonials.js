57b785fd9f6be435765595e789f354bc17177d83
codykarunas <codykarunas@gmail.com>
2019-10-25 16:12:44 -0400

    Added the parallax container to testimonial and added a linear gradient over that parallax bg image. Tweaked padding.

--- src/containers/Home/Testimonials/Testimonials.js
+++ src/containers/Home/Testimonials/Testimonials.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Container from 'react-bootstrap/Container'
+import { Parallax, Background } from 'react-parallax';
 import Carousel from 'react-bootstrap/Carousel'
 import CarouselCaption from 'react-bootstrap/CarouselCaption'
 import CarouselItem from 'react-bootstrap/CarouselItem'
@@ -30,63 +30,60 @@ export default function Testimonials() {
   ])
 
   return (
-    <Carousel className="carousel" nextIcon={nextIcon} prevIcon={prevIcon}>
-      <CarouselItem className="carousel-item">
-        <img
-          className="d-block w-100"
-          // src="https://placebear.com/640/360"
-          alt=""
-        />
-        <CarouselCaption className="carousel-caption">
-          <div>
-            <div className="carousel-caption-header">
-              What people say about our flood risk intelligence.
-            </div>
-            <hr style={{ border: "1px solid rgb(85, 185, 106)" }} />
-            <div className="carousel-caption-body">
-              <p>
-                I cannot thank you enough for this, it gives me great relief of mind that my wife and I choose the right place. Thank you!
+    <Parallax contentClassName="parallax-content" strength={700}>
+      <Background>
+        <img src="http://myfloodscore.wpengine.com/wp-content/uploads/2019/04/testimonial-image.jpg" alt="" />
+      </Background>
+      <div className="carousel-wrapper">
+        <Carousel className="carousel" nextIcon={nextIcon} prevIcon={prevIcon}>
+          <CarouselItem className="carousel-item">
+            <CarouselCaption className="carousel-caption">
+              <div>
+                <div className="carousel-caption-header">
+                  What people say about our flood risk intelligence.
+                </div>
+                <hr style={{ border: "1px solid rgb(85, 185, 106)" }} />
+                <div className="carousel-caption-body">
+                  <p>
+                    I cannot thank you enough for this, it gives me great relief of mind that my wife and I choose the right place. Thank you!
                 </p>
-            </div>
-            <div className="carousel-caption-footer">
-              <p className="testimonial-author">C Karunas</p>
-              <p>Tampa, FL</p>
-            </div>
-          </div>
-        </CarouselCaption>
-      </CarouselItem>
+                </div>
+                <div className="carousel-caption-footer">
+                  <p className="testimonial-author">C Karunas</p>
+                  <p>Tampa, FL</p>
+                </div>
+              </div>
+            </CarouselCaption>
+          </CarouselItem>
 
-      <CarouselItem className="carousel-item">
-        <img
-          className="d-block w-100"
-          // src="https://loremflickr.com/640/360"
-          alt=""
-        />
-        <CarouselCaption className="carousel-caption">
-          <div>
-            <div className="carousel-caption-header">
-              What people say about our flood risk intelligence.
+          <CarouselItem className="carousel-item">
+            <CarouselCaption className="carousel-caption">
+              <div>
+                <div className="carousel-caption-header">
+                  What people say about our flood risk intelligence.
             </div>
-            <hr style={{ border: "1px solid rgb(85, 185, 106)" }} />
-            <div className="carousel-caption-body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                <hr style={{ border: "1px solid rgb(85, 185, 106)" }} />
+                <div className="carousel-caption-body">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
                 </p>
-            </div>
-            <div className="carousel-caption-footer">
-              <p className="testimonial-author">C Karunas</p>
-              <p>Tampa, FL</p>
-            </div>
-          </div>
-        </CarouselCaption>
-      </CarouselItem>
-    </Carousel>
+                </div>
+                <div className="carousel-caption-footer">
+                  <p className="testimonial-author">C Karunas</p>
+                  <p>Tampa, FL</p>
+                </div>
+              </div>
+            </CarouselCaption>
+          </CarouselItem>
+        </Carousel>
+      </div>
+    </Parallax>
   )
 }
