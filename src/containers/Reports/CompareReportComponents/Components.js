20c73f4c4d61ba2f7d18059ffe70afb490e625e2
Kyle Hauskins-Kaplan <Kyle@Kyles-MacBook-Pro.local>
2020-01-01 21:37:33 -0500

    updates to compare report

--- src/containers/Reports/CompareReportComponents/Components.js
+++ src/containers/Reports/CompareReportComponents/Components.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import MFS_Logo from '../../../assets/images/MFS_Logo.png'
 
 export const AddFloodplainInfo = ({left, middle, right}) => (
   <Container>
@@ -18,3 +19,11 @@ export const AddFloodplainInfo = ({left, middle, right}) => (
     </Row>
   </Container>
 )
+
+export const MFSRangeImg = () => (
+  <div style={{ marginTop: '-70px', marginBottom: '30px', textAlign: 'center' }}>
+    <img style={{ position: 'relative', marginTop: '-8px', marginRight: '5px', width: '190px' }} src={MFS_Logo} />
+    <span className="bold" style={{ fontSize: '19px' }}>Range</span>
+  </div>
+)
+
